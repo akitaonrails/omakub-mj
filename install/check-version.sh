56f7219fd29480111dd2364b9916f766d5b3aaa7
AkitaOnRails <fabioakita@gmail.com>
2024-10-28 12:05:03 -0300

    adding support for arch linux, not sure if this will finish, but should allow testing

--- install/check-version.sh
+++ install/check-version.sh
@@ -8,12 +8,10 @@ fi
 
 . /etc/os-release
 
-<<<<<<< HEAD
-# Check if running on Ubuntu 24.04 or higher
-if [ "$ID" != "ubuntu" ] || [ $(echo "$VERSION_ID >= 24.04" | bc) != 1 ]; then
+if [ "$ID" != "manjaro" ]; then
   echo "$(tput setaf 1)Error: OS requirement not met"
-  echo "You are currently running: $ID $VERSION_ID"
-  echo "OS required: Ubuntu 24.04 or higher"
+  echo "You are currently running: $ID"
+  echo "OS required: Manjaro GNOME 24 or higher"
   echo "Installation stopped."
   exit 1
 fi
@@ -26,13 +24,4 @@ if [ "$ARCH" != "x86_64" ] && [ "$ARCH" != "i686" ]; then
   echo "This installation is only supported on x86 architectures (x86_64 or i686)."
   echo "Installation stopped."
   exit 1
-=======
-# Check if running on Manjaro Linux
-if [ "$ID" != "manjaro" ]; then
-    echo "$(tput setaf 1)Error: OS requirement not met"
-    echo "You are currently running: $ID"
-    echo "OS required: Manjaro GNOME 24 or higher"
-    echo "Installation stopped."
-    exit 1
->>>>>>> a9356a4 (Initial rough translation of every Ubuntu specific command into Arch/Manjaro equivalent. Not tested yet. This commit should be enough to show why we prefer the ease of use of Arch's Pacman/Yay instead of Apt)
 fi
